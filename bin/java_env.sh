20f3a97d57233b84913cfa47be9ccf8b6a636171
lihang15 <lihang15@meituan.com>
2018-12-25 23:05:17 +0800

    修改jvm参数

--- bin/java_env.sh
+++ bin/java_env.sh
@@ -4,6 +4,6 @@ export JAVA_HOME=/usr/java/jdk1.8.0_121
 export MAVEN_HOME=/home/apache-maven-3.3.9
 export PATH=${JAVA_HOME}/bin:${MAVEN_HOME}/bin:${PATH}
 
-java_max_heap_size=400M
-java_initail_heap_size=400M
+java_max_heap_size=256M
+java_initail_heap_size=256M
 java_young_heap_size=100M
